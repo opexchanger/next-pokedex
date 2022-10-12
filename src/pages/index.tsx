166da24d46123b126caa767d4561373d85476ef8
Flavio Neto <flavioneto96@gmail.com>
2022-10-12 19:37:58 -0300

    feat: basic pokemons fetch displaying their names on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import { Heading } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
 import Header from '../components/Header/Header';
+import { useAllPokemon } from '../services/pokemons';
 
 const Home: NextPage = () => {
+  const {
+    data: pokemons,
+    isLoading,
+    isError,
+  } = useAllPokemon(1, {
+    staleTime: Infinity,
+  });
+
   return (
     <>
       <Head>
@@ -15,6 +25,15 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Header />
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : isError ? (
+        <h2>Error...</h2>
+      ) : (
+        pokemons?.results.map((pokemon) => (
+          <Heading key={pokemon.name}>{pokemon.name}</Heading>
+        ))
+      )}
     </>
   );
 };
