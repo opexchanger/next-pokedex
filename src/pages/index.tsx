744bf5c507a1d9923f85532335f91de14aad91ab
Flavio Neto <flavioneto96@gmail.com>
2022-10-12 17:19:56 -0300

    chore: testando sem lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-                           <h1 className={styles.title}>
+        <h1 className={styles.title}>
           Welcome to <a href='https://nextjs.org'>Next.js!</a>
         </h1>
 
