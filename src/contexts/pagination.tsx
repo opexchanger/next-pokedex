207174507e64d1079f942a025a37fbd2095ef9d0
Flavio Neto <flavioneto96@gmail.com>
2022-10-13 10:27:30 -0300

    feat: add pagination context

---
+++ src/contexts/pagination.tsx
@@ -0,0 +1,45 @@
+import {
+  createContext,
+  useState,
+  useContext,
+  Dispatch,
+  SetStateAction,
+} from 'react';
+
+const PAGINATION_STARTS_AT = 1;
+
+export const PaginationContext = createContext<PaginationContext | undefined>(
+  undefined
+);
+
+type PaginationProviderProps = {
+  children: React.ReactNode;
+  value?: PaginationContext;
+};
+
+type PaginationContext = {
+  currentPage: number;
+  setCurrentPage: Dispatch<SetStateAction<number>>;
+};
+
+export default function PaginationProvider(props: PaginationProviderProps) {
+  const [currentPage, setCurrentPage] = useState(PAGINATION_STARTS_AT);
+
+  return (
+    <PaginationContext.Provider
+      value={{
+        currentPage,
+        setCurrentPage,
+      }}
+      {...props}
+    />
+  );
+}
+
+export function usePagination() {
+  const context = useContext(PaginationContext);
+
+  if (!context)
+    throw new Error('usePagination must be used inside a PaginationProvider');
+  return context;
+}
